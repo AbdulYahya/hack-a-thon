4bb40d38f74e55ab50ee9b2ee93918803359a780
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-01-14 02:22:16 -0800

    Cleaned up routes. Removed unnecessary code, refactored model.put's and fixed remaining routes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -43,17 +43,14 @@ public class App {
            String teamName = request.queryParams("teamName");
            String teamDesc = request.queryParams("teamDesc");
            new Team(teamName, teamDesc);
-           List<Team> teams = Team.getAll();
-           model.put("teams", teams);
-
+           model.put("teams", Team.getAll());
            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "teams.hbs"));
         });
         // get: show new member form
         get("/teams/:id/member/new", (request, response) -> {
            Map<String, Object> model = new HashMap<>();
            int teamId = Integer.parseInt(request.params("id"));
-           Team team = Team.findById(teamId);
-           model.put("team", team);
+           model.put("team", Team.findById(teamId));
            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "member-form.hbs"));
         });
         // post: process new member form
@@ -68,45 +65,41 @@ public class App {
             int age = Integer.parseInt(request.queryParams("memberAge"));
             Members member = new Members(firstName, lastName, shortDesc, age);
             team.addMember(member);
-            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "member-success.hbs"));
+            model.put("team", team);
+            model.put("members", team.getAllMembers());
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "team.hbs"));
         });
         // get: show all teams
         get("/teams", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            List<Team> teams = Team.getAll();
-
-            model.put("teams", teams);
+            model.put("teams", Team.getAll());
            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "teams.hbs"));
         });
         // get: show an individual team
         get("/teams/:id", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             int teamId = Integer.parseInt(request.params("id"));
-            Team foundTeam = Team.findById(teamId);
-            List<Members> members =  foundTeam.getAllMembers();
-            model.put("team", foundTeam);
-            model.put("members", members);
+            model.put("team", Team.findById(teamId));
             return new HandlebarsTemplateEngine().render(new ModelAndView(model, "team.hbs"));
         });
         // get: show a form to update a team
         get("/teams/:id/update", (request, response) -> {
            Map<String, Object> model = new HashMap<>();
-           int updateTeamId = Integer.parseInt(request.params("id"));
-           Team updateTeam = Team.findById(updateTeamId);
-           model.put("updateTeam", updateTeam);
-           return new HandlebarsTemplateEngine().render(new ModelAndView(model, "form.hbs"));
+           int teamId = Integer.parseInt(request.params("id"));
+           model.put("team", Team.findById(teamId));
+           return new HandlebarsTemplateEngine().render(new ModelAndView(model, "team.hbs"));
         });
         //  post: process a form to update a team
         post("/teams/:id/update", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            int updateTeamId = Integer.parseInt(request.params("id"));
-            Team updateTeam = Team.findById(updateTeamId);
+            int teamId = Integer.parseInt(request.params("id"));
+            Team updateTeam = Team.findById(teamId);
             String teamName = request.queryParams("teamName");
             String teamDesc = request.queryParams("teamDesc");
             updateTeam.setStringName(teamName);
             updateTeam.setStringDescription(teamDesc);
-            response.redirect("/teams"); // After user submits form - redirect to /teams
-            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "team.hbs"));
+            model.put("teams", Team.getAll());
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "teams.hbs"));
         });
     }
 }
