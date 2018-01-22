ae9fe3ddb1547c73c261c8b1c7f171c0f83c576e
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-01-22 08:31:27 -0800

    Added logic to deleteById. Method test passes

--- src/main/java/Dao/MemberDao.java
+++ src/main/java/Dao/MemberDao.java
@@ -72,7 +72,14 @@ public class MemberDao implements iMemberDao {
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE FROM members WHERE id = :id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
